35b10fa671f8919e583a424750867f6c4751e2f8
蔡大卫 <397080492@qq.com>
2019-04-27 21:18:03 +0800

    更新 Test02.java

--- day02/Test02.java
+++ day02/Test02.java
@@ -5,7 +5,7 @@
  */
 import java.util.Arrays;
 
-public class Demo02 {
+public class Test02 {
 	public static void main(String[] args) {
 		StringBuilder str = new StringBuilder();
 		str.append("123,456,789,012");
