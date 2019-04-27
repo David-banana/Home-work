843fddddd77129527ec978f153d2126dc43ac02b
蔡大卫 <397080492@qq.com>
2019-04-27 20:57:57 +0800

    test02

---
+++ day02/Test02.java
@@ -0,0 +1,16 @@
+/**
+ * 将字符串123,456,789,012根据","拆分，并输出拆分后的每一项
+ * @author Bonnie
+ *
+ */
+import java.util.Arrays;
+
+public class Demo02 {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder();
+		str.append("123,456,789,012");
+		String str1 = str.toString();
+		String[] str2 = str1.split(",");
+		System.out.println(Arrays.toString(str2));
+	}
+}
