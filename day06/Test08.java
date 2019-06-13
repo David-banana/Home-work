b70144b545fc69b675801a3cf2cd23477511a200
蔡大卫 <397080492@qq.com>
2019-06-13 13:55:08 +0800

    Test08.java

---
+++ day06/Test08.java
@@ -0,0 +1,15 @@
+package day06;
+
+import java.io.*;
+
+
+public class Test08 {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        File file = new File("raf.txt");
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream("raf.txt"),"utf-8"));
+        pw.println(Integer.MAX_VALUE);
+        pw.println(String.valueOf(Long.MAX_VALUE));
+        pw.flush();
+        pw.close();
+    }
+}
