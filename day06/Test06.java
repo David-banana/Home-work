342bf75d83dc05e295f952eff54123f15302b8e6
蔡大卫 <397080492@qq.com>
2019-06-13 13:53:56 +0800

    Test06.java

---
+++ day06/Test06.java
@@ -0,0 +1,21 @@
+
+
+import java.io.File;
+import java.util.LinkedList;
+
+
+public class Test06 {
+    public static void main(String[] args) {
+        String path = Test01.class.getClassLoader().getResource("day06").getFile();
+        System.out.println(path);
+        File file = new File(path + "test.txt");
+        LinkedList<String> filename = new LinkedList<String>();
+        File[] fs = file.listFiles();
+        for(int i = 0; i < fs.length; i++){
+            if(fs[i].isFile()){
+                filename.add(fs[i].getName());
+            }
+        }
+        System.out.println(filename);
+    }
+}
