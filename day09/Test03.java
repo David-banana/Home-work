09fa5b2aa851c8aeaded46a970e0af2aa62d9c70
蔡大卫 <397080492@qq.com>
2019-07-06 14:50:01 +0800

    Test03

---
+++ day09/Test03.java
@@ -0,0 +1,21 @@
+public class Test03 {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("你好");
+                }
+            }
+        }) .start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("再见");
+                }
+            }
+        }) .start();
+
+    }
+}
